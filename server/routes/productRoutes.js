4669b41d5f329f5ddb6aa91755f216048a21a4fc
yustadh <sanniyusuf9394@gmail.com>
2026-08-30 03:21:14 +0100

    Separate product controller from routes

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,27 +1,10 @@
 import express from 'express'
+import {getProducts} from '../controllers/productController.js'
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  const products = [
-    {
-      id: 1,
-      name: 'Laptop',
-      price: 1200,
-    },
-    {
-      id: 2,
-      name: 'Wireless Headphones',
-      price: 100,
-    },
-    {
-      id: 3,
-      name: 'Smartphone',
-      price: 800,
-    },
-  ]
+router.get('/', getProducts)
 
-  res.json(products)
-})
+   
 
 export default router
