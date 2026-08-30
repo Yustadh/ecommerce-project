23734881fa35b27f29c25df40dcb4288d2b63bfe
yustadh <sanniyusuf9394@gmail.com>
2026-08-30 17:39:39 +0100

    Connect backend to MongoDB

--- server/server.js
+++ server/server.js
@@ -1,17 +1,25 @@
+import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 import productRoutes from './routes/productRoutes.js'
+import { connectDB } from './config/db.js'
+
 const app = express()
 const PORT = 5000
+
 app.use(cors())
+
 app.use('/api/products', productRoutes)
+
 app.get('/', (req, res) => {
   res.send('E-commerce API is running')
 })
 
 app.get('/api/health', (req, res) => {
-  res.send('E-Commerce API is healthy')
+  res.send('API is healthy')
 })
+
+connectDB()
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`)
