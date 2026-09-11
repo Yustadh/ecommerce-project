4cbea17a04933cccf4dd9aa632cabf75bac63887
yustadh <sanniyusuf9394@gmail.com>
2026-09-11 15:38:11 +0100

    Separate Express app from server startup

---
+++ server/app.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import cors from 'cors'
+import productRoutes from './routes/productRoutes.js'
+import { errorMiddleware } from './middleware/errorMiddleware.js'
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.use('/api/products', productRoutes)
+
+app.get('/', (req, res) => {
+  res.send('E-commerce API is running')
+})
+
+app.get('/api/health', (req, res) => {
+  res.send('API is healthy')
+})
+
+app.use(errorMiddleware)
+
+export default app
--- server/server.js
+++ server/server.js
@@ -1,25 +1,9 @@
 import 'dotenv/config'
-import express from 'express'
-import cors from 'cors'
-import productRoutes from './routes/productRoutes.js'
+import app from './app.js'
 import { connectDB } from './config/db.js'
-import { errorMiddleware } from './middleware/errorMiddleware.js'
 
-const app = express()
 const PORT = 5000
-app.use(express.json())
-app.use(cors())
 
-app.use('/api/products', productRoutes)
-
-app.get('/', (req, res) => {
-  res.send('E-commerce API is running')
-})
-
-app.get('/api/health', (req, res) => {
-  res.send('API is healthy')
-})
-app.use(errorMiddleware)
 connectDB()
 
 app.listen(PORT, () => {
